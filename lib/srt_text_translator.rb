c7ec1adb0025dd66f73cd02e3813a4a334b4e42a
Shelby Kelly <shelby.kelly@colorado.edu>
2015-04-02 18:16:02 -0600

    deleted uneccessary comments

--- lib/srt_text_translator.rb
+++ lib/srt_text_translator.rb
@@ -3,7 +3,6 @@ require './lib/translator'
 class SrtTextTranslator
 
   def self.translate_srt_file(srt_info, source_lang, target_lang)
-    # element_array = SrtFormatter.extract_elements(srt_info)
     translator = Translator.new
 
     srt_info.map { |translation| translation["new_text"] = translator.get_translation(translation["text"], source_lang, target_lang) }
